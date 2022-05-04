fdad2bfe8b2a751632850c8a48847872465bbe61
Dhrvin bhuva <dhruvinbhuva777@gmail.com>
2022-05-04 22:48:16 +0530

    assignment - 4

--- Week-5/Assignment-1/assignment-1.js
+++ Week-5/Assignment-1/assignment-1.js
@@ -15,8 +15,40 @@ async function asyncAwaitFunction (praam1,param2,param3){
 }
 
 
-function generator(){
+
+//Generator Function
+function* generator(){
+
+
+    yield doTask1();
+    yield doTask2();
+    yield doTask3();
     
 }
+
+function doTask1(){
+    setTimeout(() => {
+        console.log('Generator doTask1');
+    }, 1000);
+}
+
+function doTask2(){
+    setTimeout(() => {
+        console.log('Generator doTask2');
+    }, 1000);
+}
+
+function doTask3(){
+    setTimeout(() => {
+        console.log('Generator doTask3');
+    }, 1000);
+}
+
+
+
+const gen = generator()
+gen.next();
+gen.next();
+gen.next();
 
 asyncAwaitFunction();
